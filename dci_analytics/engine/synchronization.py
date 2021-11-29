499280d687b4af62d800b03988b54519a489fa43
ylamgarchal <yassine.lamgarchal@redhat.com>
2021-11-29 12:06:00 +0100

    WIP: add full sync endpoint
    Change-Id: I8f6337de9e1c39db46c929c68b2a95b4c7958ee4

--- dci_analytics/engine/synchronization.py
+++ dci_analytics/engine/synchronization.py
@@ -36,6 +36,8 @@ def format_field(record, prefix):
         k_split = k.split("_", 1)
         if k_split[0] == prefix:
             res[k_split[1]] = v
+            if (k_split[1] == 'created_at' or k_split[1] == 'updated_at') and v is None:
+                return None
             if isinstance(v, datetime.datetime):
                 res[k_split[1]] = v.strftime("%Y-%m-%dT%H:%M:%S.%f")
     return res
@@ -47,16 +49,23 @@ def format(records):
     for r in records:
         if r["jobs_id"] not in res:
             res[r["jobs_id"]] = format_field(r, "jobs")
+            if not res[r["jobs_id"]]:
+                continue
         if "jobstates" not in res[r["jobs_id"]]:
             res[r["jobs_id"]]["jobstates"] = []
         jobstate = format_field(r, "jobstates")
+        if not jobstate:
+            continue
         if jobstate["id"] not in js:
             js[jobstate["id"]] = jobstate
             res[r["jobs_id"]]["jobstates"].append(jobstate)
         js_file = format_field(r, "files")
         if "files" not in js[jobstate["id"]]:
             js[jobstate["id"]]["files"] = []
-        js[jobstate["id"]]["files"].append(js_file)
+        if js_file:
+            js[jobstate["id"]]["files"].append(js_file)
+    if not res:
+        return []
     return list(res.values())
 
 
@@ -80,7 +89,7 @@ def get_table_columns_names(db_conn, table_name):
             sys.exit(1)
 
 
-def get_jobs(db_conn):
+def get_jobs_from_interval(db_conn, last_minutes=120):
     jobs_columns_names = get_table_columns_names(db_conn, "jobs")
     jobs_aliases = ["jobs.%s as jobs_%s" % (n, n) for n in jobs_columns_names]
     jobstates_columns_names = get_table_columns_names(db_conn, "jobstates")
@@ -89,11 +98,18 @@ def get_jobs(db_conn):
     ]
     files_columns_names = get_table_columns_names(db_conn, "files")
     files_aliases = ["files.%s as files_%s" % (n, n) for n in files_columns_names]
-    query = "SELECT %s, %s, %s from JOBS INNER JOIN JOBSTATES on (JOBSTATES.job_id = JOBS.id) INNER JOIN FILES on (FILES.jobstate_id = JOBSTATES.id) WHERE (JOBS.created_at > (current_timestamp - make_interval(mins => 120)) AND JOBS.status IN ('success', 'failure')) ORDER BY JOBSTATES.created_at ASC;"
+    query = """
+    SELECT %s, %s, %s
+    FROM JOBS
+    INNER JOIN JOBSTATES on (JOBSTATES.job_id = JOBS.id)
+    INNER JOIN FILES on (FILES.jobstate_id = JOBSTATES.id)
+    WHERE (JOBS.created_at > (current_timestamp - make_interval(mins => %s)) AND JOBS.status IN ('success', 'failure'))
+    ORDER BY JOBSTATES.created_at ASC"""
     query = query % (
         ", ".join(jobs_aliases),
         ", ".join(jobstates_aliases),
         ", ".join(files_aliases),
+        last_minutes
     )
     with db_conn.cursor(cursor_factory=pg_extras.DictCursor) as cursor:
         try:
@@ -107,10 +123,64 @@ def get_jobs(db_conn):
 
 def synchronize(_lock_synchronization):
     db_connection = get_db_connection()
-    jobs = get_jobs(db_connection)
+    jobs = get_jobs_from_interval(db_connection)
 
     for job in jobs:
         logger.info("process job %s" % job["id"])
         task_duration_cumulated.process(job)
+
+    _lock_synchronization.release()
+
+
+def get_all_jobs(db_conn, offset, limit):
+    jobs_columns_names = get_table_columns_names(db_conn, "jobs")
+    jobs_aliases_1 = ["jobs_%s" % n for n in jobs_columns_names]
+    jobs_aliases_2 = ["jobs.%s as jobs_%s" % (n, n) for n in jobs_columns_names]
+    jobstates_columns_names = get_table_columns_names(db_conn, "jobstates")
+    jobstates_aliases = [
+        "jobstates.%s as jobstates_%s" % (n, n) for n in jobstates_columns_names
+    ]
+    files_columns_names = get_table_columns_names(db_conn, "files")
+    files_aliases = ["files.%s as files_%s" % (n, n) for n in files_columns_names]
+    with db_conn.cursor(cursor_factory=pg_extras.DictCursor) as cursor:
+        query = """
+        SELECT {jobs_aliases_1}, {jobstates_aliases}, {files_aliases}
+        FROM
+        (SELECT {jobs_aliases_2}
+        FROM JOBS
+        ORDER BY JOBS.created_at ASC
+        LIMIT  {limit}
+        OFFSET {offset}) AS JOBS
+        LEFT OUTER JOIN JOBSTATES ON JOBSTATES.job_id = jobs_id LEFT OUTER JOIN FILES ON FILES.jobstate_id = JOBSTATES.id;
+        """.format(
+            jobs_aliases_1=", ".join(jobs_aliases_1),
+            jobs_aliases_2=", ".join(jobs_aliases_2),
+            jobstates_aliases=", ".join(jobstates_aliases),
+            files_aliases=", ".join(files_aliases),
+            limit=limit,
+            offset=offset
+        )
+        try:
+            cursor.execute(query)
+            jobs = [dict(j) for j in cursor.fetchall()]
+        except Exception as err:
+            print("error: %s" % str(err))
+            sys.exit(1)
+
+    return format(jobs)
+
+
+def full_synchronize(_lock_synchronization):
+    db_connection = get_db_connection()
+    limit = 100
+    offset = 0
+    while True:
+        jobs = get_all_jobs(db_connection, offset, limit)
+        if not jobs:
+            break
+        for job in jobs:
+            logger.info("process job %s" % job["id"])
+            task_duration_cumulated.process(job)
+        offset += limit
 
     _lock_synchronization.release()
